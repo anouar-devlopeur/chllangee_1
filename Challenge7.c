37c85ace5d4b407fa5d95f69d938647f6bf61da5
anouar <anouar.ab95@gmail.com>
2023-09-13 16:49:55 +0100

    frist

--- Challenge7.c
+++ Challenge7.c
@@ -2,18 +2,10 @@
 #include<stdlib.h>
 /*Challenge 7 : Nombre entier à trois chiffres en ordre inverse*/
 int main (){
-	int a,b,c,k;	
+	int n,b;	
   printf("donner un nommber  a est : ");
-  scanf("%d",&a);
-   printf("donner un nommber  b est : ");
-   scanf("%d",&b);
-     printf("donner un nommber  c est : ");
-     scanf("%d",&c);
-     k=a;
-     a=c;
-     c=k;
-       printf("affiche un nommber  a est : %d\n",a);
-           printf("affiche un nommber  b est : %d\n",b);
-               printf("affiche un nommber  c est : %d\n",c);
+  scanf("%d",&n);
+ b = (n % 10) * 100 + ((n / 10) % 10) * 10 + n / 100;
+printf("L'inverse de %d est : %d\n", n, b);
 	return 0;
 }
